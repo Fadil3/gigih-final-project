4ad33429af9404c53fb6eadac2a60df27209c406
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2023-08-14 21:27:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from "react-router-dom";
+import { Category, VideoCard } from "./components";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [videos, setVideos] = useState([]);
+  const categoryData = [
+    { title: "All", isActive: true },
+    {
+      title: "Food",
+      isActive: false,
+    },
+  ];
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/videos`)
+      .then((response) => {
+        setVideos(response.data);
+      })
+      .then((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main className="m-4">
+      <div className="mb-10 flex justify-between">
+        <h1 className="text-base font-bold text-white">Play</h1>
+        <button>
+          <svg
+            className="unf-icon"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            fill="#C7CBD2"
+            // style="display: inline-block; vertical-align: middle;"
+          >
+            <path d="m20.53 19.46-4.4-4.4a7.33 7.33 0 1 0-1.07 1.06l4.41 4.41a.77.77 0 0 0 1.06 0 .77.77 0 0 0 0-1.07Zm-15.78-9a5.75 5.75 0 1 1 5.75 5.75 5.76 5.76 0 0 1-5.75-5.72v-.03Z"></path>
+          </svg>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="mb-4 flex gap-4">
+        {categoryData.map((category, index) => {
+          return (
+            <Category
+              title={category.title}
+              isActive={category.isActive}
+              key={index}
+            />
+          );
+        })}
+      </div>
+      <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4  xl:grid-cols-6">
+        {videos?.map((video, index) => {
+          return (
+            <Link to={`/video/${video._id}`} key={index}>
+              <VideoCard
+                title={video.title}
+                toko={video.shop}
+                view={video.views}
+                thumbnail={new URL(video.thumbnail)}
+              />
+            </Link>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
